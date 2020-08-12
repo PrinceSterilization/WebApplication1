2dd82792dcfc844a17150420d3458140a4073ec1
German Yemets <gyemets@princesterilization.com>
2020-08-12 14:52:32 -0400

    Revert "Change to Hello PPl"
    This reverts commit 8d702c7c784a03770658a6931ac5c0c843dded1b.

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace WebApplication1
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello PPl!");
+                await context.Response.WriteAsync("Hello People!");
             });
         }
     }
